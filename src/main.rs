8d5c164f66408a5f97e4afe1fd93129c90fb475b
joshmeranda <joshmeranda@gmail.com>
2021-10-16 21:20:27 -0400

    implement recursively converting directory descendents

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,17 @@ use std::process::exit;
 use clap::{Arg, ArgGroup, ArgMatches};
 
 use crate::convert_path::Convention;
+use crate::error::PathConvertError;
+use walkdir::WalkDir;
 
 fn get_matches<'a>() -> ArgMatches<'a> {
     app_from_crate!()
-        // .arg(
-        //     Arg::with_name("recursive")
-        //         .help("recurse into a directory")
-        //         .short("r")
-        //         .long("recursive"),
-        // )
+        .arg(
+            Arg::with_name("recursive")
+                .help("recurse into a directory, works in the same way as using '--prefix' and '--full-path' for all sub_paths")
+                .short("r")
+                .long("recursive"),
+        )
         .arg(
             Arg::with_name("no-clobber")
                 .help("do not overwrite an existing file")
@@ -93,6 +95,86 @@ fn get_matches<'a>() -> ArgMatches<'a> {
         .get_matches()
 }
 
+fn convert_single(
+    path: &Path,
+    from: Option<Convention>,
+    to: Convention,
+    is_full_path: bool,
+    prefix: Option<&Path>,
+    is_verbose: bool,
+    is_dry_run: bool,
+    no_clobber: bool,
+) -> Result<(), PathConvertError> {
+    // todo: take this as closure rather than method
+    //       store method or closure reference outside loop or run separate loops
+    let new_path = if is_full_path {
+        if prefix.is_some() {
+            convert_path::convert_full_except_prefix(path, prefix.unwrap(), from, to)
+        } else {
+            convert_path::convert_full(path, from, to)
+        }
+    } else {
+        convert_path::convert_basename(path, from, to)
+    }?;
+
+    if !is_dry_run {
+        // todo: this conditional block is ugly
+        //       consider moving verbosity before everything with a check to avoid inaccurate verbosity logs
+        if new_path.exists() {
+            if no_clobber {
+                if is_verbose {
+                    println!("file '{}' already exists", new_path.display());
+                }
+
+                return Ok(());
+            }
+        }
+
+        let parent = new_path.parent();
+        if parent.is_some() && !parent.unwrap().exists() {
+            if let Err(err) = fs::create_dir_all(parent.unwrap()) {
+                eprintln!("Error: {}", err);
+                exit(4);
+            }
+        }
+
+        if let Err(err) = fs::rename(path, new_path.to_path_buf()) {
+            eprintln!("Error: {}", err);
+        }
+    }
+
+    if is_verbose || is_dry_run {
+        println!(
+            "'{}' -> '{}'",
+            path.to_str().unwrap(),
+            new_path.to_str().unwrap()
+        );
+    }
+
+    Ok(())
+}
+
+fn convert_recursive(
+    dir: &Path,
+    from: Option<Convention>,
+    to: Convention,
+    is_verbose: bool,
+    is_dry_run: bool,
+    no_clobber: bool,
+) -> Result<(), PathConvertError> {
+    for i in WalkDir::new(dir).contents_first(true) {
+        if let Ok(entry) = i {
+            let path = entry.path();
+
+            convert_single(
+                path, from, to, false, None, is_verbose, is_dry_run, no_clobber,
+            )?;
+        }
+    }
+
+    Ok(())
+}
+
 // todo: break this into smaller pieces
 fn main() {
     let matches = get_matches();
@@ -100,7 +182,9 @@ fn main() {
     let is_verbose = matches.is_present("verbose");
     let is_dry_run = matches.is_present("dry-run");
     let no_clobber = matches.is_present("no-clobber");
+    let is_recursive = matches.is_present("recursive");
 
+    // todo: if recursive set prefix and full-path
     let from_convention = if matches.is_present("from") {
         match Convention::try_from(matches.value_of("from").unwrap()) {
             Ok(c) => Some(c),
@@ -128,7 +212,6 @@ fn main() {
         .map(|v| {
             let path = Path::new(v);
 
-            // ensure that the path exists, and exit if it does not
             if !path.exists() {
                 eprintln!("Error: no such file or directory '{}'", path.display());
                 exit(2);
@@ -138,63 +221,33 @@ fn main() {
         })
         .collect();
 
+    let is_full_path = matches.is_present("full-path");
+    let prefix = if matches.is_present("prefix") {
+        Some(Path::new(matches.value_of("prefix").unwrap()))
+    } else {
+        None
+    };
+
     for path in paths {
-        // todo: move this check outside loop
-        //       store method or closure reference outside loop or run separate loops
-        let result = if matches.is_present("full-path") {
-            if matches.is_present("prefix") {
-                convert_path::convert_full_except_prefix(
-                    path,
-                    matches.value_of("prefix").unwrap(),
-                    from_convention,
-                    to_convention,
-                )
-            } else {
-                convert_path::convert_full(path, from_convention, to_convention)
-            }
+        if path.is_dir() && is_recursive {
+            convert_recursive(
+                path,
+                from_convention,
+                to_convention,
+                is_verbose,
+                is_dry_run,
+                no_clobber,
+            );
         } else {
-            convert_path::convert_basename(path, from_convention, to_convention)
-        };
-
-        let new_path = match result {
-            Ok(p) => p,
-            Err(err) => {
-                eprintln!("Error: {}", err);
-                exit(3);
-            }
-        };
-
-        if !is_dry_run {
-            // todo: this conditional block is UGLY
-            //       consider moving verbosity before everything with a check to avoid inaccurate verbosity logs
-            if new_path.exists() {
-                if is_verbose && no_clobber {
-                    println!("file '{}' already exists", new_path.display());
-                }
-
-                if no_clobber {
-                    continue;
-                }
-            }
-
-            let parent = new_path.parent();
-            if parent.is_some() && !parent.unwrap().exists() {
-                if let Err(err) = fs::create_dir_all(parent.unwrap()) {
-                    eprintln!("Error: {}", err);
-                    exit(4);
-                }
-            }
-
-            if let Err(err) = fs::rename(path, new_path.to_path_buf()) {
-                eprintln!("Error: {}", err);
-            }
-        }
-
-        if is_verbose || is_dry_run {
-            println!(
-                "'{}' -> '{}'",
-                path.to_str().unwrap(),
-                new_path.to_str().unwrap()
+            convert_single(
+                path,
+                from_convention,
+                to_convention,
+                is_full_path,
+                prefix,
+                is_verbose,
+                is_dry_run,
+                no_clobber,
             );
         }
     }
