f3926ed84115a95fc0a541c21a1ddb53cd6fb9d0
joshmeranda <joshmeranda@gmail.com>
2021-10-16 21:25:01 -0400

    add tests for recursive renaming

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,10 @@
-use assert_cmd::Command;
-use predicates::prelude::predicate;
 use std::fs::{self, File};
 use std::path::{Path, PathBuf};
+
+use assert_cmd::Command;
+
+use predicates::prelude::predicate;
+
 use tempfile::TempDir;
 
 fn setup(files: &[&Path], dirs: &[&Path]) -> Result<TempDir, Box<dyn std::error::Error>> {
@@ -12,8 +15,11 @@ fn setup(files: &[&Path], dirs: &[&Path]) -> Result<TempDir, Box<dyn std::error:
         let path = PathBuf::from(env_dir_path).join(file);
 
         if let Some(parent) = path.parent() {
+            // println!("=== 011 '{}' ===", parent.display());
             fs::create_dir_all(parent)?;
         }
+
+        // println!("=== 010 '{}' ---", path.clone().display());
 
         File::create(path)?;
     }
@@ -21,6 +27,7 @@ fn setup(files: &[&Path], dirs: &[&Path]) -> Result<TempDir, Box<dyn std::error:
     for dir in dirs {
         fs::create_dir_all(PathBuf::from(env_dir_path).join(dir))?;
     }
+
     Ok(env_dir)
 }
 
@@ -40,6 +47,7 @@ fn test_basename() -> Result<(), Box<dyn std::error::Error>> {
     cmd.args(&["--basename", "snake", target_path.to_str().unwrap()]);
     cmd.assert().success();
 
+    assert!(!target_path.exists());
     assert!(expected_path.exists());
 
     Ok(())
@@ -67,6 +75,7 @@ fn test_full_path() -> Result<(), Box<dyn std::error::Error>> {
     ]);
     cmd.assert().success();
 
+    assert!(!target_path.exists());
     assert!(expected_path.exists());
 
     Ok(())
@@ -95,6 +104,7 @@ fn test_full_with_parents_pre_exist() -> Result<(), Box<dyn std::error::Error>> 
     ]);
     cmd.assert().success();
 
+    assert!(!target_path.exists());
     assert!(expected_path.exists());
 
     Ok(())
@@ -122,6 +132,7 @@ fn test_full_with_parents_no_exist() -> Result<(), Box<dyn std::error::Error>> {
     ]);
     cmd.assert().success();
 
+    assert!(!target_path.exists());
     assert!(expected_path.exists());
 
     Ok(())
@@ -147,6 +158,7 @@ fn test_overwrite() -> Result<(), Box<dyn std::error::Error>> {
     ]);
     cmd.assert().success();
 
+    assert!(!target_path.exists());
     assert!(existing_path.exists());
 
     Ok(())
@@ -185,8 +197,107 @@ fn test_overwrite_no_clobber() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-// #[test]
-// fn test_recursive() -> Result<(), Box<dyn std::error::Error>> {}
+#[test]
+fn test_recursive() -> Result<(), Box<dyn std::error::Error>> {
+    let parent_dir = Path::new("Parent Dir");
+    let child_dir = parent_dir.join("Child Dir");
+    let child_file = parent_dir.join("Child File.txt");
+    let grand_child_file = child_dir.join("Grand Child File.txt");
+    let another_grand_child_file = child_dir.join("Another Grand Child File.txt");
+
+    let dir = setup(&[child_file.as_path(), grand_child_file.as_path(), another_grand_child_file.as_path()], &[])?;
+
+    let parent_dir = dir.path().join(parent_dir);
+    let child_dir = dir.path().join(child_dir);
+    let child_file = dir.path().join(child_file);
+    let grand_child_file = dir.path().join(grand_child_file);
+    let another_grand_child_file = dir.path().join(another_grand_child_file);
+
+    let mut cmd = Command::cargo_bin("ccpath")?;
+    cmd.args(&[
+        "--recursive",
+        "--prefix",
+        dir.path().to_str().unwrap(),
+        "snake",
+        parent_dir.to_str().unwrap(),
+    ]);
+
+    cmd.assert().success();
+
+    assert!(!parent_dir.exists());
+    assert!(!child_dir.exists());
+    assert!(!child_file.exists());
+    assert!(!grand_child_file.exists());
+    assert!(!another_grand_child_file.exists());
+
+    assert!(dir.path().join("parent_dir").exists());
+    assert!(dir
+        .path()
+        .join("parent_dir")
+        .join("child_file.txt")
+        .exists());
+    assert!(dir.path().join("parent_dir").join("child_dir").exists());
+    assert!(dir
+        .path()
+        .join("parent_dir")
+        .join("child_dir")
+        .join("grand_child_file.txt")
+        .exists());
+    assert!(dir
+        .path()
+        .join("parent_dir")
+        .join("child_dir")
+        .join("another_grand_child_file.txt")
+        .exists());
+
+    Ok(())
+}
+
+#[test]
+fn test_dir_no_recursive() -> Result<(), Box<dyn std::error::Error>> {
+    let parent_dir = Path::new("Parent Dir");
+    let child_dir = parent_dir.join("Child Dir");
+    let child_file = parent_dir.join("Child File.txt");
+    let grand_child_file = child_dir.join("Grand Child File.txt");
+
+    let dir = setup(&[child_file.as_path(), grand_child_file.as_path()], &[])?;
+
+    let parent_dir = dir.path().join(parent_dir);
+    let child_dir = dir.path().join(child_dir);
+    let child_file = dir.path().join(child_file);
+    let grand_child_file = dir.path().join(grand_child_file);
+
+    let mut cmd = Command::cargo_bin("ccpath")?;
+    cmd.args(&[
+        "--prefix",
+        dir.path().to_str().unwrap(),
+        "snake",
+        parent_dir.to_str().unwrap(),
+    ]);
+
+    cmd.assert().success();
+
+    assert!(!parent_dir.exists());
+    assert!(!child_dir.exists());
+    assert!(!child_file.exists());
+    assert!(!grand_child_file.exists());
+
+    assert!(dir.path().join("parent_dir").exists());
+    assert!(dir
+        .path()
+        .join("parent_dir")
+        .join("Child File.txt")
+        .exists());
+    assert!(dir.path().join("parent_dir").join("Child Dir").exists());
+    assert!(dir
+        .path()
+        .join("parent_dir")
+        .join("Child Dir")
+        .join("Grand Child File.txt")
+        .exists());
+
+    Ok(())
+}
 
 #[test]
 fn test_no_path() -> Result<(), Box<dyn std::error::Error>> {
